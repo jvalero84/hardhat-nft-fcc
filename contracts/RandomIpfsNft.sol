a849fc2ab1aefc405dbd540dee7a37090a17612d
jvalero <jvalero84@gmail.com>
2023-11-03 22:06:41 +0100

    Finished Random IPFS Nft (included fee for minter and possibility to withdraw funds by the owner)

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -5,10 +5,13 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 error RandomIpfsNft__RangeOutOfBounds();
+error RandomIpfsNft__NeedMoreETHSent();
+error RandomIpfsNft__TransferFailed();
 
-contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage {
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
     // When we mint an NFT, we will trigger a ChainLink VRF call to give us a random number
     // using that number, we will get a random NFT
     // Between Pug, Shiba Inu and St.Bernard. and each will have a different degree of rarity..
@@ -39,23 +42,33 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage {
     uint256 private s_tokenCounter;
     uint256 internal constant MAX_CHANCE_VALUE = 100;
     string[] internal s_dogTokenUris;
+    uint256 internal immutable i_mintFee;
+
+    // Events
+    event NftRequested(uint256 indexed requestId, address requester);
+    event NftMinted(Breed dogBreed, address minter);
 
     constructor(
         address vrfCoordinatorV2,
         uint64 subscriptionId,
         bytes32 gasLane,
         uint32 callbackGasLimit,
-        string[3] memory dogTokenUris
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
+        string[3] memory dogTokenUris,
+        uint256 mintFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") Ownable(msg.sender) {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
         i_callbackGasLimit = callbackGasLimit;
         s_dogTokenUris = dogTokenUris;
+        i_mintFee = mintFee;
     }
 
     // To kick a VRF Chainlink request
-    function requestNft() public returns (uint256 requestId) {
+    function requestNft() public payable returns (uint256 requestId) {
+        if (msg.value < i_mintFee) {
+            revert RandomIpfsNft__NeedMoreETHSent();
+        }
         requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
@@ -64,6 +77,7 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage {
             NUM_WORDS
         );
         s_requestIdToSender[requestId] = msg.sender;
+        emit NftRequested(requestId, msg.sender);
         return requestId;
     }
 
@@ -80,6 +94,15 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage {
         Breed dogBreed = getBreedFromModdedRng(moddedRng);
         _safeMint(dogOwner, newTokenId);
         _setTokenURI(newTokenId, s_dogTokenUris[uint256(dogBreed)]);
+        emit NftMinted(dogBreed, dogOwner);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        if (!success) {
+            revert RandomIpfsNft__TransferFailed();
+        }
     }
 
     function getBreedFromModdedRng(uint256 moddedRng) public pure returns (Breed) {
@@ -98,5 +121,18 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage {
         return [10, 30, MAX_CHANCE_VALUE];
     }
 
-    function tokenURI(uint256 /*tokenId*/) public view override returns (string memory) {}
+    // Not needed because we are extending ERC721URIStorage and calling _setTokenUri ourselves
+    // function tokenURI(uint256 /*tokenId*/) public view override returns (string memory) {}
+
+    function getMintFee() public view returns (uint256) {
+        return i_mintFee;
+    }
+
+    function getDogTokenUris(uint256 index) public view returns (string memory) {
+        return s_dogTokenUris[index];
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
